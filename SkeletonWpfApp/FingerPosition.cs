b3243038dcc8175883be36bb090f7fff7c068470
Massimo <mabonann@microsoft.com>
2017-12-06 19:10:58 +0100

    Add skeleton sample

---
+++ SkeletonWpfApp/FingerPosition.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Microsoft.Gestures;
+using Microsoft.Gestures.Skeleton;
+
+namespace SkeletonWpfApp
+{
+    public class FingerPosition
+    {
+        private Canvas Canvas;
+        private Dispatcher Dispatcher;
+        private Finger Finger;
+        private FrameworkElement UIElement;
+
+        private float MaxX = 200;
+        private float MinX = -200;
+        private float MaxY = 200;
+        private float MinY = -100;
+
+        public FingerPosition(Canvas canvas, Dispatcher dispatcher, FrameworkElement uiElement, Finger finger)
+        {
+            this.Canvas = canvas;
+            this.Dispatcher = dispatcher;
+            this.Finger = finger;
+            this.UIElement = uiElement;
+        }
+
+    public void UpdatePosition(IHandSkeleton handSkeleton)
+    {
+        Dispatcher.Invoke(() =>
+        {
+            var fingerPosition = handSkeleton.FingerPositions[this.Finger];
+
+            var deltaXSource = MaxX - MinX;
+            var deltaXDest = (float)Canvas.ActualWidth;
+            var xDest = (fingerPosition.X - MinX) * deltaXDest / deltaXSource;
+
+            var deltaYSource = MaxY - MinY;
+            var deltaYDest = (float)Canvas.ActualHeight;
+            var yDest = (fingerPosition.Y - MinY) * deltaYDest / deltaYSource;
+
+            Canvas.SetRight(UIElement, xDest);
+            Canvas.SetTop(UIElement, yDest);
+        });
+    }
+}
+}
---
+++ SkeletonWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Gestures;
+using Microsoft.Gestures.Endpoint;
+using Microsoft.Gestures.Stock.HandPoses;
+
+namespace SkeletonWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GesturesServiceEndpoint _gesturesService;
+
+        private FingerPosition IndexPosition;
+        private FingerPosition ThumbPosition;
+        private FingerPosition MiddlePosition;
+        private FingerPosition RingPosition;
+        private FingerPosition PinkyPosition;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += WindowLoaded;
+            Closed += (s, args) => _gesturesService.Dispose();
+
+        }
+        private async void WindowLoaded(object sender, RoutedEventArgs windowLoadedArgs)
+        {
+            IndexPosition = new FingerPosition(this.Canvas, this.Dispatcher, this.IndexEllipse, Finger.Index);
+            ThumbPosition = new FingerPosition(this.Canvas, this.Dispatcher, this.ThumbEllipse, Finger.Thumb);
+            MiddlePosition = new FingerPosition(this.Canvas, this.Dispatcher, this.MiddleEllipse, Finger.Middle);
+            RingPosition = new FingerPosition(this.Canvas, this.Dispatcher, this.RingEllipse, Finger.Ring);
+            PinkyPosition = new FingerPosition(this.Canvas, this.Dispatcher, this.PinkyEllipse, Finger.Pinky);
+
+            _gesturesService = GesturesServiceEndpointFactory.Create();
+            await _gesturesService.ConnectAsync();
+
+            await _gesturesService.RegisterToSkeleton(SkeletonHandler);
+        }
+
+        private void SkeletonHandler(object sender, HandSkeletonsReadyEventArgs e)
+        {
+            IndexPosition.UpdatePosition(e.DefaultHandSkeleton);
+            ThumbPosition.UpdatePosition(e.DefaultHandSkeleton);
+            MiddlePosition.UpdatePosition(e.DefaultHandSkeleton);
+            RingPosition.UpdatePosition(e.DefaultHandSkeleton);
+            PinkyPosition.UpdatePosition(e.DefaultHandSkeleton);
+        }
+    }
+}
